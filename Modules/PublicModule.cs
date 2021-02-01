59226ed3a211c4e0eb817f41698816a15f942394
Martijn v.d. Linden <m-vd-linden@hotmail.com>
2021-02-01 10:33:03 +0100

    Added Chris
    Added the Chris Discord Bot, without the execution of choosing de beroerdste.
    
    commands are working on this commit

---
+++ Modules/PublicModule.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chris.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        [RequireOwner]
+        public Task PingAsync()
+            => ReplyAsync("pong!");
+
+        [Command("userinfo")]
+        [RequireOwner]
+        public async Task UserInfoAsync(IUser user = null)
+        {
+            user = user ?? Context.User;
+
+            await ReplyAsync(user.ToString());
+        }
+
+        [Command("echo")]
+        [RequireOwner]
+        public Task EchoAsync([Remainder] string text)
+            => ReplyAsync('\u200B' + text);
+    }
+}
