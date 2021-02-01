37601ff73ddbe8898a7611c38f09b700484e8174
Martijn v.d. Linden <m-vd-linden@hotmail.com>
2021-02-01 10:33:03 +0100

    Added Chris
    Added the Chris Discord Bot, without the execution of choosing de beroerdste

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Chris.Services;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Yaml;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chris
+{
+    class Program
+    {
+        private IConfigurationRoot Configuration { get; set; }
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddYamlFile("_config.yml");
+            Configuration = builder.Build();
+
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+
+                client.Log += LogAsync;
+
+                await services.GetRequiredService<StartupService>().StartAsync();
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                await Task.Delay(Timeout.Infinite);
+            }
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<StartupService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .AddSingleton(Configuration)
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ Services/StartupService.cs
@@ -0,0 +1,41 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chris.Services
+{
+    public class StartupService
+    {
+        private readonly IServiceProvider _provider;
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+
+        public StartupService(IServiceProvider provider,
+            DiscordSocketClient discord,
+            CommandService commands,
+            IConfigurationRoot config)
+        {
+            _provider = provider;
+            _config = config;
+            _discord = discord;
+            _commands = commands;
+        }
+
+        public async Task StartAsync()
+        {
+            string token = _config["tokens:Pietoe"];
+
+            if (string.IsNullOrWhiteSpace(token))
+                throw new Exception("Token incorrect");
+
+            await _discord.LoginAsync(Discord.TokenType.Bot, token);
+            await _discord.StartAsync();
+        }
+    }
+}
